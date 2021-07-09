cdab261b025cd8751ce09db3b61df23db6962fa2
Simone de santis <simone.agentelazio@yahoo.it>
2021-07-09 14:58:17 +0200

    inserimento messaggio solo se ha uno sconto

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ va applicato uno sconto del 40 % per gli over 65.
 L'output del prezzo finale va stampato in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 */
 
-console.log("js ok");
+// console.log("js ok");
 
 
 
@@ -14,19 +14,43 @@ var distanceToTravel = parseInt(prompt("distanza da percorrere(km)"))
 var passengerAge = parseInt(prompt("età del passeggero(anni compiuti)"))
 var ticketPrice = 0.21
 var ticketPriceTotal = ""
-console.log("distanza da percorrere:", distanceToTravel)
-console.log("età passegero:", passengerAge)
-console.log("prezzo biglietto per km :", ticketPrice)
+var discountMessage = ("Ti è stato applicato uno sconto del: ")
+// console.log("distanza da percorrere:", distanceToTravel)
+// console.log("età passegero:", passengerAge)
+// console.log("prezzo biglietto per km :", ticketPrice)
 
+// discount logic
+var hasDiscount = false
+
+// ----------------------------
 if (passengerAge < 18) {
-    ticketPriceTotal = (distanceToTravel * ticketPrice) - (((distanceToTravel * ticketPrice) * 20) / 100)
+    ticketPriceTotal = (distanceToTravel * ticketPrice) - (((distanceToTravel * ticketPrice) * 20) / 100);
+    discountMessage += "20 %"
+    var hasDiscount = true
+
 } else if (passengerAge > 64) {
     ticketPriceTotal = (distanceToTravel * ticketPrice) - (((distanceToTravel * ticketPrice) * 40) / 100)
+    discountMessage += "40 %"
+    var hasDiscount = true
+
 } else {
     ticketPriceTotal = distanceToTravel * ticketPrice
 }
 
-console.log("prezzo finale con offerta applicata", ticketPriceTotal);
+// inserimento messaggio discount in html
+// console.log(hasDiscount)
+if (hasDiscount == true) {
+    var inDiscountMessage = document.getElementById("discount-message");
+    inDiscountMessage.innerHTML = discountMessage
+
+}
+
+
+
+
+
+
+// console.log("prezzo finale con offerta applicata", ticketPriceTotal);
 
 var inTotalPrice = document.getElementById("total-price-ticket");
 inTotalPrice.innerHTML = ticketPriceTotal.toFixed(2);
@@ -37,5 +61,5 @@ inKm.innerHTML = distanceToTravel;
 var inAge = document.getElementById("age");
 inAge.innerHTML = passengerAge;
 
-var n = ticketPriceTotal.toFixed(2)
+
 
